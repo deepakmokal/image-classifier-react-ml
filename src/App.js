d9af6eb4e143e808f0a15a5eaa89a0bd2adcf9d4
deepakmokal <deepak.mokal@think.design>
2022-03-15 22:51:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+// import img from './assets/dog.jpg'
 import './App.css';
+import * as ml5 from "ml5";
+import {useEffect, useState} from 'react'
 
 function App() {
+  const [imageURL, setImageURL] = useState('')
+  const [prediction, setPrediction] = useState({
+    label: '',
+    confidence: '',
+  })
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    mobileNetClassfier();
+  };
+
+  const handleInputChange = e => {
+    
+    setImageURL(e.target.value);
+  };
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  const handleOnChange = () => {
+    setImageURL(`https://source.unsplash.com/random/400x400?sig=${getRandomInt(10)}`);
+    
+    setTimeout( ()=>{
+      mobileNetClassfier();
+    }, 300);
+
+  }
+
+  function mobileNetClassfier() {
+   let mobilenet = ml5.imageClassifier('MobileNet', ()=> {
+      
+      const image = document.getElementById('imgsrc')
+      
+      mobilenet.predict(image, (err, data)=> {
+        if(err){
+          console.error(err)
+        }
+        else {
+          
+          setPrediction({
+            label: data[0].label,
+            confidence: (Math.round((data[0].confidence) * 100) / 100).toFixed(2) * 100 + '%'
+          });
+          
+        }
+      })
+    })
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3 className='mb-5 text-primary'>Image Recognition using ML5</h3>
+        <div className="image-box shadow-lg p-3 card mb-4">
+          <img crossOrigin="anonymous" id='imgsrc' src={imageURL} alt=" not found" />
+        </div>
+      <div className='d-flex justify-content-center boder mb-4'>
+        <input className='form-control ' onChange={handleInputChange} type="text" value={imageURL} id='imgURL' placeholder='Ener Image URL' />
+        <button className='btn btn-primary ml-2' onClick={handleSubmit}>Submit</button> <br/>
+      </div>
+      
+      <button className='btn btn-warning' onClick={handleOnChange} type="text" id='' >Load Random Image</button>
+        <p className='text-muted mt-4'>The Above Image Is Predicted as <span className='text-info font-weight-bold text-capitalize'>{prediction.label}</span> With Confidence of <span className='text-info font-weight-bold'>{prediction.confidence}</span>  </p>
+        
     </div>
   );
 }
